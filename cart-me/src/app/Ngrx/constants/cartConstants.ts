e27c0e8635c714b5f27c424d489782bab992ebbe
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-03-15 06:39:10 +0100

    cart

--- cart-me/src/app/Ngrx/constants/cartConstants.ts
+++ cart-me/src/app/Ngrx/constants/cartConstants.ts
@@ -1,3 +1,3 @@
-export const ADD_CART_ITEM = "ADD_CART_ITEM";
-export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM";
-export const CART_EMPTY = "CART_EMPTY";
+export const ADD_CART_ITEM = "[cart page] ADD_CART_ITEM";
+export const REMOVE_CART_ITEM = "[cart page]  REMOVE_CART_ITEM";
+export const CART_EMPTY = "[cart page] CART_EMPTY";
