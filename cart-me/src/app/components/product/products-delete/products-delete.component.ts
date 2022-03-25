4665a79c59425410550db336b07f110ec29e8ce2
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-03-25 11:00:09 +0100

    updates

--- cart-me/src/app/components/product/products-delete/products-delete.component.ts
+++ cart-me/src/app/components/product/products-delete/products-delete.component.ts
@@ -25,7 +25,6 @@ export class ProductsDeleteComponent implements OnInit {
       this.prodId = data.id
     })
 
-    this.deleteProduct()
   }
 
   //#########################################################################################
