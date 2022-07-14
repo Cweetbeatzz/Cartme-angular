4a60d83f1472b30bb4b7cf49bf6b7cd678a73085
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-07-14 13:14:39 +0100

    upddates

--- cart-me/src/app/components/search/search.component.ts
+++ cart-me/src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/services/Product/products.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchList:any
+  error:any
+
+  constructor(private router:Router,private productApi:ProductsService) { }
+
+  //###############################################################
 
   ngOnInit(): void {
+  }
+
+  //###############################################################
+
+  onSubmit(form:NgForm){
+    this.router.navigate(['search',form.value.search])
+  }
+
+  //###############################################################
+
+  searchProduct(sort:string, searchName:string){
+    this.productApi.GetAllProduct().subscribe({
+      next:(response)=>{
+       this.searchList = response
+       console.log("Response is visible")
+      },
+      error:(err)=>{
+        this.error = err
+       console.log("Error might be visible")
+
+      },
+      complete:()=>{}
+    })
   }
 
 }
